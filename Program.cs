1db1993a3a5fe5bc768be67b03d78d0e6b704f71
Diego Katz <diekatz@gmail.com>
2023-11-18 12:42:28 -0300

    terminado y verificado

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_Primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, divisor, numDivisores = 0;
+            
+            for (numero = 2; numero <= 100; numero++) 
+            {
+                for (divisor = 1; divisor <= numero; divisor++) 
+                {
+                    if(numero %  divisor == 0) 
+                    {
+                        numDivisores += 1;
+                    }
+                }
+                if(numDivisores <= 2)
+                {
+                    Console.WriteLine(numero);
+                }
+                numDivisores = 0;
+            }
+        }
+    }
+}
